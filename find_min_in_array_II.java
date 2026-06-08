a378e450d4ca96bf6d5739e8fdb44f64322d12f7
NikhilKushwaha9899 <kushwahanikhil616@gmail.com>
2026-06-08 17:00:46 +0530

    LeetCode #154(Hard)

---
+++ find_min_in_array_II.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class find_min_in_array_II {
+    public int findMin(int[] nums){
+     Arrays.sort(nums);
+        int result=nums[0];
+        return result;
+    }
+    public static void main(String[] args) {
+        find_min_in_array fa =new find_min_in_array();
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        System.out.println(fa.findMin(nums));
+    }
+}
