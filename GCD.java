0aa4b7ec87db252b4bbe8b3caa31776a0d1e50ec
Nikhilkushwaha9899 <kushwahanikhil616@gmail.com>
2026-07-16 18:17:36 +0530

    LeetCode #3658(easy)

---
+++ GCD.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class GCD{
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+    public static void main(String[] args) {
+        GCD gc=new GCD();
+        int n=3;
+        System.out.println(gc.gcdOfOddEvenSums(n));
+    }
+}
