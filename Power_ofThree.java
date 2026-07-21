8aa4bf4b71a9aa56fadbf39332aa264377167ebb
Nikhilkushwaha9899 <kushwahanikhil616@gmail.com>
2026-07-21 18:38:13 +0530

    LeetCode #326(easy)

---
+++ Power_ofThree.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Power_ofThree {
+public boolean isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+    public static void main(String[] args) {
+        Power_ofThree pot=new Power_ofThree();
+        int n=59049;
+        System.out.println(pot.isPowerOfThree(n));
+    }
+}
