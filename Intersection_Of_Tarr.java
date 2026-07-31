34f462f5706e123bd925c960a72d75aae106d81c
Nikhilkushwaha9899 <kushwahanikhil616@gmail.com>
2026-07-31 19:02:38 +0530

    LeetCode #349(easy)

---
+++ Intersection_Of_Tarr.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Intersection_Of_Tarr {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        List<Integer> l = new ArrayList<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                if (l.isEmpty() || l.get(l.size() - 1) != nums1[i]) {
+                    l.add(nums1[i]);
+                }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int arr[] = new int[l.size()];
+        for (int x = 0; x < arr.length; x++) {
+            arr[x] += l.get(x);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Intersection_Of_Tarr iot = new Intersection_Of_Tarr();
+        int nums1[] = { 1, 2, 2, 1 };
+        int nums2[] = { 2, 2 };
+        System.out.println(Arrays.toString(iot.intersection(nums1, nums2)));
+    }
+}
