bc4672d0d7644edf2ad2efddc16245b480c5dd43
Nikhilkushwaha9899 <kushwahanikhil616@gmail.com>
2026-09-02 19:07:21 +0530

    LeetCode #896(easy)

--- Monotonic_array.java
+++ Monotonic_array.java
@@ -1,16 +1,25 @@
 public class Monotonic_array {
     public boolean isMonotonic(int[] nums) {
-        boolean small=true;
-		boolean big=true;
-		for(int i=1;i<nums.length;i++){
-			if(nums[i-1]>nums[i]){
-				small=false;
-			}
-			if(nums[i]<nums[i-1]){
-				big=false;
-			}
-		}
-		return small || big;
+        int n = nums.length;
+        if (n == 1) return true;
+
+        boolean isInc = true;
+        boolean isDec = true;
+
+        for (int i = 1; i < n; i++) {
+            if (!isInc && !isDec) {
+                return false;
+            }
+
+            if (nums[i] < nums[i - 1]) {
+                isInc = false;
+            }
+            if (nums[i] > nums[i - 1]) {
+                isDec = false;
+            }
+        }
+
+        return isInc || isDec;        
     }
     public static void main(String[] args) {
         Monotonic_array ma=new Monotonic_array();
